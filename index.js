227debb43716025db32002210943a1632d4566c7
unknown <ashishmoreee123@gmail.com>
2025-10-25 08:36:22 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Welcome to the DevOps module!");
+console.log("I am learning about CI/CD pipelines, infrastructure as code, and monitoring.");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting DevOps tests in 3 seconds...");
+setTimeout(() => {
+    console.log("DevOps tests started!");
+}, 3000);
+console.log("DevOps tests completed!");
